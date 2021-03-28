88c8b012edeb5b12a9ea132cbb7b820da428654a
Alessandro Satriano <alessandro.satriano@gmail.com>
2021-03-28 00:40:53 -0600

    ...

--- expresstrain/model.py
+++ expresstrain/model.py
@@ -393,7 +393,7 @@ class ExpressTrain:
             print(f"Epoch {epoch+1}/{epochs}, {self.metric_used.__name__}_train: {self.train_metric_list[-1]:.2f}")
             if self.valid_loader is not None:
 
-                print(f"--- Epoch {epoch+1}/{epochs}, {self.metric_used.__name__}_valid: {self.val_metric_list[-1]:.2f}")
+                print(f"Epoch {epoch+1}/{epochs}, {self.metric_used.__name__}_valid: {self.val_metric_list[-1]:.2f}")
             
             if self.save_every is not None:
 
