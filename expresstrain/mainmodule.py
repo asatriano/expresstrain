0b0689bf321ffd2fd32a84f6d452c1ca4671837d
as-deeplearning <alessandro.satriano.ai@gmail.com>
2021-03-31 20:41:48 -0600

    introduced messaging hooks

--- expresstrain/mainmodule.py
+++ expresstrain/mainmodule.py
@@ -368,6 +368,8 @@ class ExpressTrain:
         on_train_epoch_begin, on_train_epoch_end
         on_valid_epoch_begin, on_valid_epoch_end
         on_test_epoch_begin, on test_epoch_end'''
+        
+        self.epochs=epochs
         if self.fp16==True:
             self.scaler=torch.cuda.amp.GradScaler()
         else:
@@ -408,9 +410,7 @@ class ExpressTrain:
                 if (self.lr_adjuster_on_val is not None) and (self.valid_loader is not None):
                     self.lr_adjust_on_val()
             
-            print(f"\nEpoch {epoch+1}/{epochs}, {self.metric_used.__name__}_train: {self.train_metric_list[-1]:.2f}")
-            if self.valid_loader is not None:
-                print(f"Epoch {epoch+1}/{epochs}, {self.metric_used.__name__}_valid: {self.val_metric_list[-1]:.2f}")
+            self.print_progress()
             
             if self.save_every is not None:
 
@@ -440,8 +440,17 @@ class ExpressTrain:
             self.test_metric_list.append(metric_epoch)
             self.on_test_end()
 
-            print(f"\nInference on holdout: {self.metric_used.__name__}_test: {self.test_metric_list[-1]:.2f}")
+            self.print_progress(inference_on_holdout=True)
 
+    def print_progress(self, inference_on_holdout=False):
+        if inference_on_holdout==False:
+            print(f"\nEpoch {self.epoch+1}/{self.epochs}, {self.metric_used.__name__}_train: {self.train_metric_list[-1]:.2f}")
+            if self.valid_loader is not None:
+                print(f"Epoch {self.epoch+1}/{self.epochs}, {self.metric_used.__name__}_valid: {self.val_metric_list[-1]:.2f}")
+        else:
+            if self.test_loader is not None:
+                print(f"Epoch {self.epoch+1}/{self.epochs}, {self.metric_used.__name__}_valid: {self.test_metric_list[-1]:.2f}")
+    
     def on_batch_begin(self):
         pass
 
