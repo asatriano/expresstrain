735ced12cf85d7a64bc8cfd68c2f87910df46d28
as-deeplearning <alessandro.satriano.ai@gmail.com>
2021-04-02 04:27:04 -0600

    added Tensorboard logger

--- expresstrain/utils/logger.py
+++ expresstrain/utils/logger.py
@@ -13,6 +13,9 @@ class ExpressLogger(object):
     
     def initialize_writer(self):
         return NotImplemented
+
+    def log_one_var(self, phase, epoch, var_name, var_value):
+        return NotImplemented
     
     def write_data(self, **kwargs):
         return NotImplemented
@@ -23,6 +26,18 @@ class ExpressLogger(object):
     def close(self):
         return NotImplemented
 
+    def check_and_log(self, phase, epoch, var_name, var_value):
+        if isinstance(var_name, list):
+            assert(isinstance(var_value, list))
+            assert(len(var_name)==len(var_value))
+            for var_idx, var in enumerate(var_name):
+                self.log_one_var(phase, epoch,
+                    var, var_value[var_idx])
+        else:
+            assert(isinstance(var_value, list)==False)
+            self.log_one_var(phase, epoch,
+                var_name, var_value)
+
 # %%
 class ExpressTensorBoard(ExpressLogger):
     def __init__(self, log_dir=None, comment='', purge_step=None,
@@ -32,17 +47,20 @@ class ExpressTensorBoard(ExpressLogger):
 
         self.writer=torch.utils.tensorboard.SummaryWriter(
             log_dir, comment, purge_step, max_queue, flush_secs, filename_suffix)
+        
+    def log_one_var(phase, epoch, var_name, var_value)
+        self.writer.add_scalar(f"{var_name}/{phase}",
+                                var_value, epoch,
+                                )
 
-    
+    def write_data(self, phase, epoch, loss_name=None, loss_value=None, metric_name=None, metric_value=None):
+        self.check_and_log(phase, epoch, loss_name, loss_value)
+        self.check_and_log(phase, epoch, metric_name, metric_value)
 
+    def flush(self):
+        self.writer.flush
 
+    def close(self):
+        self.writer.close()
 
-
-# - generate logsession -> self.writer
-# - Save to logsession(self, phase)
-# - Flush logsession
-# - Close logsession
-
-# Class expresstensor(expresslog):
-# — superinit
-# -subclass the rest
+# %%
